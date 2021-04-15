25d41e9385451589a4a203179ab966cee35ef383
Kacper Rozdolski <k.rpzdolski@gmail.com>
2021-04-15 15:49:44 +0200

    Navigation

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,10 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap");
 body {
   margin: 0 !important;
+  font-family: Lato;
 }
 .earth-foundation {
   min-height: 100vh;
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,5 +1,20 @@
 <template>
-  <section></section>
+  <section>
+    <nav>
+      <div class="left">
+        <span>Home</span><span>About</span><span>Projects</span
+        ><span>Partners</span>
+      </div>
+      <div class="center">
+        <span>Earth Foundation</span>
+        <img src="../assets/logo.svg" />
+      </div>
+      <div class="right">
+        <span>Mission</span><span>News</span><span>FAQ</span><span>Share</span>
+      </div>
+    </nav>
+    <main></main>
+  </section>
 </template>
 
 <script>
@@ -12,5 +27,48 @@ export default {
 section {
   min-height: 100vh;
   width: 100%;
+  background-image: url("../assets/heroImage.png");
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  nav {
+    height: 10vh;
+    width: 90%;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    color: white;
+    .left {
+      width: 30%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+    .center {
+      min-width: 20%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 10px;
+      span {
+        font-size: 2.5rem;
+      }
+      img {
+        height: 100%;
+      }
+    }
+    .right {
+      width: 30%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  }
+  main {
+    height: 90vh;
+    width: 90%;
+  }
 }
 </style>
